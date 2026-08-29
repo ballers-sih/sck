c119d5f8e999d4df5f98568b7172cd59301ad168
Shounak996 <shounakd19@gmail.com>
2026-08-29 23:52:47 +0530

    Removal of debugging statement
    Removal of debugging statement

--- cmds/sck_imapd/__main__.py
+++ cmds/sck_imapd/__main__.py
@@ -10,7 +10,6 @@ load_dotenv(dotenv_path=ENV_PATH)
 mail = imaplib.IMAP4_SSL("imap.gmail.com")
 EMAIL_USER = os.environ.get("EMAIL_USER")
 EMAIL_PASS = os.environ.get("EMAIL_PASS")
-print(f"What Python reads -> User: '{EMAIL_USER}' | Pass: '{EMAIL_PASS}'")
 mail.login(EMAIL_USER, EMAIL_PASS)
 
 mail.select("INBOX")
