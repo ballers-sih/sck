8af6c1c7a433d8edb17a9599a9597d3bab3c1f5c
Shounak996 <shounakd19@gmail.com>
2026-08-29 23:38:34 +0530

    Log email user and password for debugging
    Added print statement to display email user and password.

--- cmds/sck_imapd/__main__.py
+++ cmds/sck_imapd/__main__.py
@@ -10,6 +10,7 @@ load_dotenv(dotenv_path=ENV_PATH)
 mail = imaplib.IMAP4_SSL("imap.gmail.com")
 EMAIL_USER = os.environ.get("EMAIL_USER")
 EMAIL_PASS = os.environ.get("EMAIL_PASS")
+print(f"What Python reads -> User: '{EMAIL_USER}' | Pass: '{EMAIL_PASS}'")
 mail.login(EMAIL_USER, EMAIL_PASS)
 
 mail.select("INBOX")
