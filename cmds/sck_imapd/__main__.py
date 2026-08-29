6f23e26e081a913cd3de16e153bc9073e63d6e7f
Shounak996 <shounakd19@gmail.com>
2026-08-29 22:35:59 +0530

    Change email storage directory to XDG_CACHE_DIR
    .eml files are sent over to XDG_CACHE_DIR for clean storage

--- cmds/sck_imapd/__main__.py
+++ cmds/sck_imapd/__main__.py
@@ -10,14 +10,15 @@ mail.select("INBOX")
 status, messages = mail.search(None, "ALL")
 email_ids = messages[0].split()
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-SCKD_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", "sckd"))
+xdg_cache = os.environ.get("XDG_CACHE_HOME", os.path.expanduser("~/.cache"))
+XDG_CACHE_DIR = os.path.join(xdg_cache, "sck_app")
+os.makedirs(XDG_CACHE_DIR, exist_ok=True) 
 
 for idx, email_id in enumerate(email_ids):
     res, msg_data = mail.fetch(email_id, "(RFC822)")
     for response_part in msg_data:
         if isinstance(response_part, tuple):
-            file_path = os.path.join(SCKD_DIR, f"email_{idx}.eml")
+            file_path = os.path.join(XDG_CACHE_DIR, f"email_{idx}.eml")
             with open(file_path, "wb") as f:
                 f.write(response_part[1])
 
